3715b4be423d981db68831982a1dee50a783eaf9
ishan <ishan4220@gmail.com>
2021-06-17 19:47:30 +0530

    edit_in_local_storage

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ var editRow = null;
 let localDataStorage = localStorage.getItem("data");
 if (!localDataStorage) localStorage.setItem("data", JSON.stringify([]));
 
+var list = null;
+var heading = null;
+var button = null;
+var form = null;
+
 window.onload = function () {
+
+  list = document.getElementsByClassName("list-container")[0];
+  heading = document.getElementById("add-form-heading");
+  button = document.getElementById("add-form-button");
+  form = document.getElementById("list-form");
+
   loadExistingData();
-  let form = document.getElementById("list-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     let mode = form.getAttribute("data-mode");
@@ -14,107 +24,122 @@ window.onload = function () {
       editRowHandler();
     }
   });
-};
 
-function addRowHandler() {
-  let itemName = document.getElementById("item-name");
-  let quantity = document.getElementById("quantity").value;
-  let list = document.getElementsByClassName("list-container")[0];
-  let index = JSON.parse(localStorage.getItem("data")).length;
-  let listRow = createNewRow(itemName.innerText, quantity, index);
-  list.appendChild(listRow);
-  let data = JSON.parse(localStorage.getItem("data"));
-  data.push({ name: itemName.innerText, quantity: quantity });
-  localStorage.removeItem("data");
-  localStorage.setItem("data", JSON.stringify(data));
-  itemName.innerText = "";
-}
-
-function editEnableHandler(listRow) {
-  let form = document.getElementById("list-form");
-  editRow = listRow;
-  form.setAttribute("data-mode", "edit");
-  let index = this.getAttribute("data-index");
-  let data = JSON.parse(localStorage.getItem("data"));
-  let itemName = document.getElementById("item-name");
-  let quantity = document.getElementById("quantity").value;
-
-  itemName.innerText = data[index].name;
-  quantity.value = data[index].quantity;
-}
-
-function editRowHandler() {
-  if (editRow) {
+  function addRowHandler() {
     let itemName = document.getElementById("item-name");
     let quantity = document.getElementById("quantity").value;
-    let itemNameRow = editRow.childNodes[0];
-    itemNameRow.innerText = itemName.innerText;
-    let form = document.getElementById("list-form");
-    form.setAttribute("data-mode", "add");
-    itemName.innerText = "";
-    editRow = null;
-  } else {
-    alert("Internal Server Error");
-  }
-}
-
-function loadExistingData() {
-  let data = JSON.parse(localStorage.getItem("data"));
-  let list = document.getElementsByClassName("list-container")[0];
-  let index = 0;
-  for (let i in data) {
-    let listRow = createNewRow(data[i].name, data[i].quantity, index);
+    let index = JSON.parse(localStorage.getItem("data")).length;
+    let listRow = createNewRow(itemName.innerText, quantity, index);
     list.appendChild(listRow);
-    index++;
+    let data = JSON.parse(localStorage.getItem("data"));
+    data.push({ name: itemName.innerText, quantity: quantity });
+    localStorage.removeItem("data");
+    localStorage.setItem("data", JSON.stringify(data));
+    itemName.innerText = "";
   }
-}
 
-function createNewRow(name, quantity, index) {
-  let listRow = document.createElement("div");
-  listRow.setAttribute("class", "list-row");
+  function editEnableHandler(listRow) {
+   
+    editRow = listRow;
+    let index = this.getAttribute("data-index");
 
-  let itemNameRow = document.createElement("div");
-  itemNameRow.setAttribute("class", "item-name");
-  itemNameRow.setAttribute("id", "item-name-" + index);
-  itemNameRow.innerText = name;
+    form.setAttribute("data-mode", "edit");
+    form.setAttribute("data-index", index);
 
-  let editButton = document.createElement("button");
-  editButton.setAttribute("class", "btn-edit");
-  editButton.setAttribute("type", "button");
-  editButton.setAttribute("data-index", index);
-  editButton.innerText = "Edit";
+    
+    let data = JSON.parse(localStorage.getItem("data"));
+    let itemName = document.getElementById("item-name");
+    let quantity = document.getElementById("quantity").value;
 
-  editButton.addEventListener(
-    "click",
-    editEnableHandler.bind(editButton, listRow)
-  );
+    heading.innerText = "Edit Grocery Item";
+    button.innerText = "Edit Item";
 
-  let deleteButton = document.createElement("button");
-  deleteButton.setAttribute("class", "btn-danger");
-  deleteButton.innerText = "Delete";
+    itemName.innerText = data[index].name;
+    quantity.value = data[index].quantity;
+  }
 
-  editButton.setAttribute("data-index", index);
-  deleteButton.setAttribute("data-index", index);
+  function editRowHandler() {
+    if (editRow) {
+      let itemName = document.getElementById("item-name");
+      let quantity = document.getElementById("quantity").value;
+      let itemNameRow = editRow.childNodes[0];
 
-  deleteButton.addEventListener(
-    "click",
-    deleteHandler.bind(deleteButton, listRow)
-  );
+      itemNameRow.innerText = itemName.innerText;
 
-  listRow.appendChild(itemNameRow);
-  listRow.appendChild(editButton);
-  listRow.appendChild(deleteButton);
-  return listRow;
-}
+      let index = form.getAttribute("data-index");
 
-function deleteHandler(listRow) {
-  let list = document.getElementsByClassName("list-container")[0];
-  list.removeChild(listRow);
-  let data = JSON.parse(localStorage.getItem("data"));
-  let index = this.getAttribute("data-index");
-  let finalArray = data.filter(function (el, i) {
-    return i != index;
-  });
-  localStorage.removeItem("data");
-  localStorage.setItem("data", JSON.stringify(finalArray));
-}
+      form.setAttribute("data-mode", "add");
+
+      let data = JSON.parse(localStorage.getItem("data"));
+      data[index] = { name: itemName.innerText, quantity: quantity };
+      localStorage.removeItem("data");
+      localStorage.setItem("data", JSON.stringify(data));
+
+      itemName.innerText = "";
+      editRow = null;
+      heading.innerText = "Add Grocery Item";
+      button.innerText = "Add Item";
+    } else {
+      alert("Internal Server Error");
+    }
+  }
+
+  function loadExistingData() {
+    let data = JSON.parse(localStorage.getItem("data"));
+    let index = 0;
+    for (let i in data) {
+      let listRow = createNewRow(data[i].name, data[i].quantity, index);
+      list.appendChild(listRow);
+      index++;
+    }
+  }
+
+  function createNewRow(name, quantity, index) {
+    let listRow = document.createElement("div");
+    listRow.setAttribute("class", "list-row");
+
+    let itemNameRow = document.createElement("div");
+    itemNameRow.setAttribute("class", "item-name");
+    itemNameRow.setAttribute("id", "item-name-" + index);
+    itemNameRow.innerText = name;
+
+    let editButton = document.createElement("button");
+    editButton.setAttribute("class", "btn-edit");
+    editButton.setAttribute("type", "button");
+    editButton.setAttribute("data-index", index);
+    editButton.innerText = "Edit";
+
+    editButton.addEventListener(
+      "click",
+      editEnableHandler.bind(editButton, listRow)
+    );
+
+    let deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "btn-danger");
+    deleteButton.innerText = "Delete";
+
+    editButton.setAttribute("data-index", index);
+    deleteButton.setAttribute("data-index", index);
+
+    deleteButton.addEventListener(
+      "click",
+      deleteHandler.bind(deleteButton, listRow)
+    );
+
+    listRow.appendChild(itemNameRow);
+    listRow.appendChild(editButton);
+    listRow.appendChild(deleteButton);
+    return listRow;
+  }
+
+  function deleteHandler(listRow) {
+    list.removeChild(listRow);
+    let data = JSON.parse(localStorage.getItem("data"));
+    let index = this.getAttribute("data-index");
+    let finalArray = data.filter(function (el, i) {
+      return i != index;
+    });
+    localStorage.removeItem("data");
+    localStorage.setItem("data", JSON.stringify(finalArray));
+  }
+};
